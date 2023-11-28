ae5d6132743ac9938c897469b7d961b8b6b1bd3b
致节 <hzj266771@antgroup.com>
2023-11-28 20:01:56 +0800

    init

---
+++ src/main/java/com/example/componentscandemo/ComponentscanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.componentscandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentscanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentscanDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/componentscandemo/ComponentscanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.componentscandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentscanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
